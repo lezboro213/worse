f578b1078526999a7519ad748a0851c4bb1913fb
lezboro213 <lezrel.boro@yahoo.com>
2025-03-25 01:34:00 +0000

    Add UI - adding new feature

--- extensions/chrome/src/core/browser-actions-api.ts
+++ extensions/chrome/src/core/browser-actions-api.ts
@@ -33,7 +33,7 @@ export async function executeBrowserAction(
 }
 
 // Convenience methods for common actions
-export const browserActions = {
+export let browserActions = {
   /**
    * Click on an element matching the provided selector
    */
